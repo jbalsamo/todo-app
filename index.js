97acf230aac88b8efd39743bad09c29e49e48671
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2023-03-22 11:42:07 -0400

    fixed and broke

--- index.js
+++ index.js
@@ -33,13 +33,43 @@ const isHideChecked = () => {
   return checked;
 };
 
+const toggleTodo = (id) => {
+  let raw = JSON.stringify({
+    query: `
+    mutation Mutation($toggleTodoId: ID!) {
+      toggleTodo(id: $toggleTodoId) {
+        code
+        success
+        message
+        todo {
+          id
+          desc
+          done
+        }
+      }
+    }`,
+    variables: {
+      id: id,
+    },
+  });
+
+  let requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:4000/", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+  location.reload();
+};
+
 const isHiddenSet = () => {
   let hidden = window.localStorage.getItem("hideDone");
-  if (!hidden) {
-    return false;
-  } else {
-    return true;
-  }
+  return !hidden;
 };
 
 const url = "http://localhost:4000";
@@ -67,12 +97,29 @@ fetch(url, requestOptions)
     let todos = result.data.todos;
     console.log(todos);
     todos.forEach((element) => {
-      console.log(element);
-      let checked = element.done ? "" : "checked";
+      let hide = document.getElementById("hd").value;
+      console.log(hide);
+      let checked = element.done == true ? "checked" : "";
+      console.log("🚀 ~ file: index.js:72 ~ todos.forEach ~ checked:", checked);
       let node = document.createElement("li");
       let checkbox =
-        "<input type='checkbox' " + "id=" + element.id + " " + checked + "/> ";
+        "<input type='checkbox' " +
+        "id=" +
+        element.id +
+        " " +
+        checked +
+        " onclick=\"toggleTodo('" +
+        element.id +
+        "')\" /> ";
+      console.log(
+        "🚀 ~ file: index.js:106 ~ todos.forEach ~ checkbox:",
+        checkbox
+      );
       node.innerHTML = checkbox + element.desc;
+      console.log(
+        "🚀 ~ file: index.js:80 ~ todos.forEach ~ node.innerHTML:",
+        node.innerHTML
+      );
       list.appendChild(node);
     });
   })
